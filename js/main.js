d6b042465e52ce10ee6945e7230036e73466f337
hiro-tsu <hiroyuki14tsuchiya@gmail.com>
2019-06-29 11:47:11 +0900

    Math.randomでの乱数生成テスト。コメントアウト済

--- js/main.js
+++ js/main.js
@@ -13,4 +13,13 @@
         btn.classList.remove('pressed');
     });
 
+
+
+    /*Math.randomを使った乱数生成テスト*/
+    // let lan_int = Math.floor(Math.random() * 5);
+    // let lan_int = Math.floor(Math.random() * (5 + 1));
+    // let lan_int = 5 + Math.floor(Math.random() * (5 + 1));
+    // let lan_int = 5 + Math.floor(Math.random() * (10 + 1 -5));
+    // console.log(lan_int);
+
 }
