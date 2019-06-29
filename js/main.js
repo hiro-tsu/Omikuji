5b5997803b8128223af6656e2c2d578517e2916d
hiro-tsu <hiroyuki14tsuchiya@gmail.com>
2019-06-29 12:09:45 +0900

    配列を利用したおみくじバージョン

--- js/main.js
+++ js/main.js
@@ -4,19 +4,26 @@
     const btn = document.getElementById('btn');
     
     btn.addEventListener('click', () => {
-        const n = Math.floor(Math.random() * 3);
+        const results = ['大吉', '中吉', '凶', '小吉'];
+        const n = Math.floor(Math.random() * results.length);
         btn.textContent = n;
-        switch(n) {
-            case 0:
-                btn.textContent = '大吉';
-                break;
-            case 1:
-                btn.textContent = '中吉';
-                break;
-            case 2:
-                btn.textContent = '凶';
-                break;
-        }
+
+        btn.textContent = results[n];
+        
+        /*switch構文版*/
+        // switch(n) {
+        //     case 0:
+        //         btn.textContent = '大吉';
+        //         break;
+        //     case 1:
+        //         btn.textContent = '中吉';
+        //         break;
+        //     case 2:
+        //         btn.textContent = '凶';
+        //         break;
+        // }
+
+        
     });
     btn.addEventListener('mousedown', () => {
         btn.classList.add('pressed');
